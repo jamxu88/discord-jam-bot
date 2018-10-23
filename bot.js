bd7b657474a734b8a48b2c144826d6eaeccecd24
James <43918220+jamxu88@users.noreply.github.com>
2018-10-23 18:41:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("ready", () => {
   console.log("Jam-Bot Online");
-  client.user.setActivity("say =help for help", { type: "PLAYING"});
 });
 const prefix = "=";
 client.on("message", (message) => {
