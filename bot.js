0b4f7f6a651ba9ab32cdee5f4aba19e9c76c31ce
James <43918220+jamxu88@users.noreply.github.com>
2018-10-29 20:12:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,6 +54,7 @@ client.on("message", (message) => {
   }
   if (x = 100) {
     message.channel.send("**Our Partners**: \n `Discord Ads` - <https://discord.gg/48axjnP> \n `Naihvoom40` - <https://www.twitch.tv/naihvoom40> \n `Idle-Empire` - <https://www.idle-empire.com/r/discord> \n **Our Media**: \n `Instagram` - <https://www.instagram.com/vs2tgaming/> \n `Twitter` - <https://twitter.com/VS2T_Gaming> \n `Steam Group` - <https://steamcommunity.com/groups/VS2T> \n `Steam Curator` - <https://store.steampowered.com/curator/31900195/> \n `Jam's Steam Profile` - <https://steamcommunity.com/id/jaaaaaaaam> \n `Spig's Steam Profile` - <https://steamcommunity.com/id/RavenPotato> \n `Gold's Steam Profile` - <https://steamcommunity.com/id/VS2TGOLD>");
+    x = 0
   }
 });
 client.login(process.env.BOT_TOKEN);
