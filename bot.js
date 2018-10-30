5f5525ecbe9ee713c9ca08da44b98c6a4eb176e2
James <43918220+jamxu88@users.noreply.github.com>
2018-10-29 20:09:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,11 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("ready", () => {
   console.log("Jam-Bot Online");
+  x = 0
 });
 const prefix = "=";
 client.on("message", (message) => {
+  x = x + 1
   if (!message.content.startsWith(prefix)) return;
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   if (message.content.startsWith(prefix + "alert")) {
@@ -48,6 +50,9 @@ client.on("message", (message) => {
     message.channel.send("yeah jam doesn't know how to do that just yet");
   }else
   if (message.content.startsWith(prefix + "info")) {
+    message.channel.send("**Our Partners**: \n `Discord Ads` - <https://discord.gg/48axjnP> \n `Naihvoom40` - <https://www.twitch.tv/naihvoom40> \n `Idle-Empire` - <https://www.idle-empire.com/r/discord> \n **Our Media**: \n `Instagram` - <https://www.instagram.com/vs2tgaming/> \n `Twitter` - <https://twitter.com/VS2T_Gaming> \n `Steam Group` - <https://steamcommunity.com/groups/VS2T> \n `Steam Curator` - <https://store.steampowered.com/curator/31900195/> \n `Jam's Steam Profile` - <https://steamcommunity.com/id/jaaaaaaaam> \n `Spig's Steam Profile` - <https://steamcommunity.com/id/RavenPotato> \n `Gold's Steam Profile` - <https://steamcommunity.com/id/VS2TGOLD>");
+  }
+  if (x = 100) {
     message.channel.send("**Our Partners**: \n `Discord Ads` - <https://discord.gg/48axjnP> \n `Naihvoom40` - <https://www.twitch.tv/naihvoom40> \n `Idle-Empire` - <https://www.idle-empire.com/r/discord> \n **Our Media**: \n `Instagram` - <https://www.instagram.com/vs2tgaming/> \n `Twitter` - <https://twitter.com/VS2T_Gaming> \n `Steam Group` - <https://steamcommunity.com/groups/VS2T> \n `Steam Curator` - <https://store.steampowered.com/curator/31900195/> \n `Jam's Steam Profile` - <https://steamcommunity.com/id/jaaaaaaaam> \n `Spig's Steam Profile` - <https://steamcommunity.com/id/RavenPotato> \n `Gold's Steam Profile` - <https://steamcommunity.com/id/VS2TGOLD>");
   }
 });
