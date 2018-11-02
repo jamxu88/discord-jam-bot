9fe90cf5251f60692ce7ff876cd827dca70b37e6
James <43918220+jamxu88@users.noreply.github.com>
2018-11-01 20:40:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,10 @@ if (x == 200) {
   message.channel.send("`Hey! Support our Discord by checking out our stuff:` \n **Our Partners**: \n `Discord Ads` - <https://discord.gg/48axjnP> \n `Naihvoom40` - <https://www.twitch.tv/naihvoom40> \n `Idle-Empire` - <https://www.idle-empire.com/r/discord> \n **Our Media**: \n `Instagram` - <https://www.instagram.com/vs2tgaming/> \n `Twitter` - <https://twitter.com/VS2T_Gaming> \n `Steam Group` - <https://steamcommunity.com/groups/VS2T> \n `Steam Curator` - <https://store.steampowered.com/curator/31900195/> \n `Jam's Steam Profile` - <https://steamcommunity.com/id/jaaaaaaaam> \n `Spig's Steam Profile` - <https://steamcommunity.com/id/RavenPotato> \n `Gold's Steam Profile` - <https://steamcommunity.com/id/VS2TGOLD>");
   x = 0
 }else
+  if (x % 3 == 0) {
+    message.react("🤔")
+  }else
   x = x + 1
-  message.react("🤔")
   if (!message.content.startsWith(prefix)) return;
   if (!message.content.startsWith(prefix) || message.author.bot) return;
     if (message.content.startsWith(prefix + "alert")) {
